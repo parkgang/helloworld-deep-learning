2b9e976c62a1f17f2e8e90231e94c0adff32e339
Kyungeun <ruddms936@naver.com>
2021-03-22 01:24:52 +0900

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+#%%
+class man:
+  def __init__(self):
+    print('Initialized!');
+  def helloDavid(self):
+    print('Hello David!');
+  def goodbyeDavid(self):
+    print('Good-bye David!');
+m = man();
+m.helloDavid();
+m.goodbyeDavid();
+# %%
