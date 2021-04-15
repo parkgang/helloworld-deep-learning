3c3a984575cfc0f188814e59e24b20d95c61ccd7
Kyungeun <ruddms936@naver.com>
2021-04-15 23:22:10 +0900

    add: 데이터 셋 가져오기

--- 7/main.py
+++ 7/main.py
@@ -4,4 +4,9 @@ import keras as k
 
 print("텐서플로우 버전 = ", tf.__version__)
 print("케라스 버전 = ", k.__version__)
+#%%
+from keras.datasets import cifar10
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
 # %%
