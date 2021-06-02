b7d7d077e1e77389c9ae3ee7f44e2cfa25c8df74
박경은 <ruddms936@naver.com>
2021-06-03 06:45:27 +0900

    add: 합성곱 신경망 만들기

--- 13/main.py
+++ 13/main.py
@@ -1,4 +1,4 @@
-#%%
+# %% 패션 MNIST 데이터 불러오기
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 
@@ -9,4 +9,6 @@ train_scaled = train_input.reshape(-1, 28, 28, 1) / 255.0
 
 train_scaled, val_scaled, train_target, val_target = train_test_split(
     train_scaled, train_target, test_size=0.2, random_state=42)
+# %% 합성곱 신경망 만들기
+model = keras.Sequential()
 # %%
